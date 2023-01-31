f1f6e75aa719fb04c45f32feb49f3bc583ebbc10
mdstevens044 <mdstevens044@gmail.com>
2023-01-31 05:08:09 -0500

    Added navbar to house substack link

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import About from "../components/About";
 import Services from "../components/Services";
 import Team from "../components/Team";
 import Twitter from "../components/Twitter";
+import NavBar from "../components/NavBar";
 import Layout from "../components/Layout/Layout";
 
 export default function Home() {
   return (
     <>
       <Layout>
+        <NavBar />
         <Hero />
         <About />
         <Services />

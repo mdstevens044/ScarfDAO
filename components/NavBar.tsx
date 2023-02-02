326de0714a4ccaf27d3e95e66b9547b9d722c232
mdstevens044 <mdstevens044@gmail.com>
2023-02-01 23:51:32 -0500

    Trying to figure out why the nav bar isn't working in production

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,7 +7,7 @@ function NavBar() {
     <nav className="bg-white shadow">
       <div className="flex-1 pb-3 mt-4 pr-4 md:pr-8">
         <ul className="items-end justify-end">
-          <li className="underline text-right">
+          <li className="underline text-right text-black">
             <a href="https://asknoot.substack.com/"  target="_blank" rel="noopener noreferrer">Substack</a>
           </li>
         </ul>

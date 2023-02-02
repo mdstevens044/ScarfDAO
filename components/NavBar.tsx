b924e8b51661a7f423bb2c88e547facc692d416c
mdstevens044 <mdstevens044@gmail.com>
2023-02-02 02:49:05 -0500

    I don't know

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,12 +5,8 @@ function NavBar() {
 
   return (
     <nav className="shadow">
-      <div className="flex-1 pb-3 mt-4 pr-4 md:pr-8">
-        <ul className="items-end justify-end">
-          <li className="text-right">
-            <a className="underline text-black" href="https://asknoot.substack.com/"  target="_blank" rel="noopener noreferrer">Substack</a>
-          </li>
-        </ul>
+      <div className="flex-1 pb-3 mt-4 pr-4 md:pr-8 text-right">
+        <a className="underline text-black" href="https://asknoot.substack.com/"  target="_blank" rel="noopener noreferrer">Substack</a>
       </div>
     </nav>
   );

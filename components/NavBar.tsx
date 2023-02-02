d906e1e3975558398ae188881701276c055ad96a
mdstevens044 <mdstevens044@gmail.com>
2023-02-02 01:01:13 -0500

    Try and figure out why link in navbar is white

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,8 +7,8 @@ function NavBar() {
     <nav className="shadow">
       <div className="flex-1 pb-3 mt-4 pr-4 md:pr-8">
         <ul className="items-end justify-end">
-          <li className="underline text-right text-black">
-            <a href="https://asknoot.substack.com/"  target="_blank" rel="noopener noreferrer">Substack</a>
+          <li className="text-right">
+            <a className="underline text-black" href="https://asknoot.substack.com/"  target="_blank" rel="noopener noreferrer">Substack</a>
           </li>
         </ul>
       </div>

72958764defaaddb863f1dc950103956a2f03354
mdstevens044 <mdstevens044@gmail.com>
2023-02-02 03:26:31 -0500

    I don't know

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,10 +4,8 @@ function NavBar() {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="shadow">
-      <div className="flex-1 pb-3 mt-4 pr-4 md:pr-8 text-right">
-        <a className="underline text-black" href="https://asknoot.substack.com/"  target="_blank" rel="noopener noreferrer">Substack</a>
-      </div>
+    <nav className="flex-1 pb-3 mt-4 pr-4 md:pr-8 text-right">
+      <a className="underline text-black" href="https://asknoot.substack.com/"  target="_blank" rel="noopener noreferrer">Substack</a>
     </nav>
   );
 }
